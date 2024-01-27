d82c165613657388b77bda90cd56f6bb6b7e4547
Jasmin B. Maglic <jmaglic@outlook.de>
2024-01-27 22:06:22 +0100

    Add copyright notice for interactor class
    Found a copyright notice for the code in an older reporitory:
    https://github.com/vadz/wxVTK
    
    I added the copyright notice back into the cxx and h files and added a
    LICENSE file to reproduce the VTK license under which the code is
    seemingly licensed. Also added a license file for my work.

--- wxVTKRenderWindowInteractor.cxx
+++ wxVTKRenderWindowInteractor.cxx
@@ -1,3 +1,24 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Language:  C++
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+===========================================================================
+
+  This software has been modified in accordance with the VTK license.
+
+  Copyright: (c) 2024 Jasmin B. Maglic
+
+=========================================================================*/ 
+
 #include "wxVTKRenderWindowInteractor.h"
 #include <vtkCommand.h>
 #include <vtkDebugLeaks.h>
